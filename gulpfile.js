35cc68411d71a9af23f959e98304690ab49665a1
alienat3d <psy.alienated@gmail.com>
2021-08-09 18:42:17 +0300

    finished video-fashon sect

--- gulpfile.js
+++ gulpfile.js
@@ -33,10 +33,11 @@ function styles() {
 
 function scripts() {
   return src([
-      "node_modules/jquery/dist/jquery.js",
-      "node_modules/slick-carousel/slick/slick.js",
-      "app/js/main.js"
-    ])
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/@fancyapps/ui/dist/fancybox.umd.js",
+    "app/js/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js/"))
