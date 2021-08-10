1ca6fee3c3de3e02112cd1cc46d3ae80cfe36879
alienat3d <psy.alienated@gmail.com>
2021-08-10 05:08:17 +0300

    finished promo and partners

--- app/js/main.js
+++ app/js/main.js
@@ -17,4 +17,49 @@ $(function() {
     readOnly: true,
   });
   // === / RATEYO PLUGIN ===
+
+  // === COUNTDOWN ===
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector(".promo__countdown");
+    const daysSpan = clock.querySelector(".promo__countdown-days");
+    const hoursSpan = clock.querySelector(".promo__countdown-hours");
+    const minutesSpan = clock.querySelector(".promo__countdown-minutes");
+    const secondsSpan = clock.querySelector(".promo__countdown-seconds");
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $(".promo__countdown").attr('data-time');
+  initializeClock(".promo__countdown", deadline);
+  // === / COUNTDOWN ===
 });
