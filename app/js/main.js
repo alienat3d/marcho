fbe46f33fd3320227bea69d75719e4eee22edfb1
alienat3d <psy.alienated@gmail.com>
2021-12-14 13:36:29 +0300

    half-made blog.html

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,8 @@ $(function () {
     normalFill: '#ccccce',
     ratedFill: '#ffc35b',
     readOnly: true,
+    starSvg:
+      '<svg width="18" height="16"><path d="m8.102.555-2.04 4.14-4.566.664C.68 5.477.352 6.49.946 7.066l3.3 3.22-.781 4.546c-.14.82.726 1.438 1.45 1.05L9 13.739l4.086 2.145c.723.383 1.59-.23 1.45-1.051l-.782-4.547 3.3-3.219c.594-.578.266-1.59-.55-1.707l-4.567-.664L9.899.555c-.367-.735-1.425-.746-1.796 0Zm0 0"/></svg>',
   });
   // === / RATEYO PLUGIN ===
 
@@ -82,7 +84,17 @@ $(function () {
   $('.button-grid').on('click', function () {
     $('.product-card').removeClass('product-card--list');
   });
-  // === / LIST FILTER JS ===
+  // === / LIST FILTER JQ ===
+
+  // === BLOG SLIDER JQ ===
+  $('.blog-page__slider').slick({
+    infinite: false,
+    prevArrow:
+      '<button type="button" class="slick-prev"><svg width="8" height="16"><path d="m.992 7.469 4.25-4.25a.747.747 0 0 1 1.059 0l.707.707a.747.747 0 0 1 0 1.058L3.996 8l3.012 3.012a.74.74 0 0 1 0 1.058l-.703.711a.752.752 0 0 1-1.063 0l-4.25-4.25a.745.745 0 0 1 0-1.062Zm0 0"></svg></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg width="8" height="16"><path d="m7.008 8.531-4.25 4.25a.747.747 0 0 1-1.059 0l-.707-.707a.747.747 0 0 1 0-1.058l3.016-3.012L.992 4.992a.752.752 0 0 1 0-1.062l.703-.711a.752.752 0 0 1 1.063 0l4.25 4.25a.745.745 0 0 1 0 1.062Zm0 0"></svg></button>',
+  });
+  // === / BLOG SLIDER JQ ===
 
   // === COUNTDOWN JS ===
   function getTimeRemaining(endtime) {
